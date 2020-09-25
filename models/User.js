1e2ee55fac182578527230d168b529d1dc4f5171
narae-yoon <nare6557@gmail.com>
2020-09-26 05:04:07 +0900

    Add password encryption on User DB

--- models/User.js
+++ models/User.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10; // salt의 길이
 
 // schema 생성
 const userSchema = mongoose.Schema({
@@ -31,6 +33,25 @@ const userSchema = mongoose.Schema({
     // token의 유효기간
     type: Number,
   },
+});
+
+// save 메소드가 실행되기 전에 수행할 함수 정의
+userSchema.pre('save', function (next) {
+  let user = this;
+
+  if (user.isModified('password')) {
+    //salt를 만든 후 salt를 이용해 비밀번호 암호화
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+
+        user.password = hash;
+        next();
+      });
+    });
+  }
 });
 
 // userSchema가 포함된 User 모델 생성
