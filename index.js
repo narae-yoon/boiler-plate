14195b3de2ff70a76ed154b3dc3b81dae4a8c90c
narae-yoon <nare6557@gmail.com>
2020-09-26 03:42:05 +0900

    Add mongoDB

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const express = require('express'); // express 모듈 가져오기
 const app = express(); // 새로운 express app 만들기
 const port = 5000;
 
+// mongoDB 연결
+const mongoose = require('mongoose');
+mongoose
+  .connect(
+    'mongodb+srv://narae:narae@cluster0.un05t.mongodb.net/narae-db?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log('MongoDB Connected...'))
+  .catch((err) => console.log(err));
+
 // root 디렉토리에 Hello World! 출력
 app.get('/', (req, res) => res.send('Hello World!'));
 
